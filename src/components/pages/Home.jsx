dba78f73efa56d88cd1b64ee416a0dda1c2dcd7a
Sebastian <sebastian.onofrei.business@gmail.com>
2023-10-11 15:49:42 +0300

    update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../organisms/header/Header'
 import Hero from '../organisms/hero/Hero'
 import OurServices from '../organisms/our-services/OurServices';
 import WhoAreWe from '../organisms/who-are-we/WhoAreWe';
+import OurLocation from '../organisms/our-location/OurLocation';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
 			<Hero />
 			<OurServices/>
 			<WhoAreWe/>
+			<OurLocation/>
 		</>
 	);
 }
